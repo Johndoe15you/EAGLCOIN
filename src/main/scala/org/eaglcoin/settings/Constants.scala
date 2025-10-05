9cdbfb92aebf1d725da2088f4a7c34012c171f3e
Johndoe15you <rockerjd143@gmail.com>
2025-10-05 18:48:01 -0400

    Create Constants.scala
    Add Constants.scala with EAGLCOIN chain parameters

---
+++ src/main/scala/org/eaglcoin/settings/Constants.scala
@@ -0,0 +1,10 @@
+package org.eaglcoin.settings
+
+object Constants {
+  val ChainId: String = "EAGL"
+  val CoinName: String = "EAGLCOIN"
+  val CoinTicker: String = "EAGL"
+  val InitialDifficulty: Long = 1
+  val BlockIntervalSeconds: Int = 120
+  val GenesisTimestamp: Long = 0L // epoch start
+}
