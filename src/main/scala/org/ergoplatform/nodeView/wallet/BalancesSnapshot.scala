ddeab0261a7087c87c4996170e84e0bf82a8517a
Johndoe15you <rockerjd143@gmail.com>
2025-10-05 20:35:03 -0400

    Delete src/main/scala/org/ergoplatform directory
    to help with conflicts in testnet

--- src/main/scala/org/ergoplatform/nodeView/wallet/BalancesSnapshot.scala
+++
@@ -1,6 +0,0 @@
-package org.ergoplatform.nodeView.wallet
-
-import org.ergoplatform.nodeView.history.ErgoHistoryUtils.Height
-import org.ergoplatform.sdk.wallet.TokensMap
-
-final case class BalancesSnapshot(height: Height, balance: Long, assetBalances: TokensMap)
--- src/main/scala/org/ergoplatform/settings/Args.scala
+++
@@ -1,7 +0,0 @@
-package org.ergoplatform.settings
-
-final case class Args(userConfigPathOpt: Option[String] = None, networkTypeOpt: Option[NetworkType] = None)
-
-object Args {
-  def empty: Args = Args(None, None)
-}
